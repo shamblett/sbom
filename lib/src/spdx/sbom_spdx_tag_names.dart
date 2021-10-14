0d03a5bf9b456d4736f71866e9b4e5be2e326aac
Steve Hamblett <steve.hamblett@linux.com>
2021-10-14 16:47:15 +0100

    SPDX package section tag names

--- lib/src/spdx/sbom_spdx_tag_names.dart
+++ lib/src/spdx/sbom_spdx_tag_names.dart
@@ -9,10 +9,13 @@ part of sbom;
 
 /// SPDX tag names as defined by the SPDX specification.
 class SbomSpdxTagNames {
+
+  // Common
+  static const String identifier = 'SPDXID';
+
   // Document creation
   static const String version = 'SPDXVersion';
   static const String dataLicense = 'DataLicense';
-  static const String identifier = 'SPDXID';
   static const String documentName = 'DocumentName';
   static const String documentNamespace = 'DocumentNamespace';
   static const String externalDocumentRefs = 'ExternalDocumentRef';
@@ -21,4 +24,29 @@ class SbomSpdxTagNames {
   static const String created = 'Created';
   static const String creatorComment = 'CreatorComment';
   static const String documentComment = 'DocumentComment';
+
+  // Package
+  static const String packageName = 'PackageName';
+  static const String packageVersion = 'PackageVersion';
+  static const String packageFileName = 'PackageFileName';
+  static const String packageSupplier = 'PackageSupplier';
+  static const String packageOriginator = 'PackageOriginator';
+  static const String packageDownloadLocation = 'PackageDownloadLocation';
+  static const String packageFilesAnalyzed = 'FilesAnalyzed';
+  static const String packageVerificationCode = 'PackageVerificationCode';
+  static const String packageChecksum = 'PackageChecksum';
+  static const String packageHomePage = 'PackageHomePage';
+  static const String packageSourceInfo = 'PackageSourceInfo';
+  static const String packageLicenseConcluded = 'PackageLicenseConcluded';
+  static const String packageLicenseInfoFromFiles = 'PackageLicenseInfoFromFiles';
+  static const String packageLicenseDeclared = 'PackageLicenseDeclared';
+  static const String packageLicenseComments = 'PackageLicenseComments';
+  static const String packageCopyrightText = 'PackageCopyrightText';
+  static const String packageSummary = 'PackageSummary';
+  static const String packageDescription = 'PackageDescription';
+  static const String packageComment = 'PackageComment';
+  static const String packageExternalRef = 'ExternalRef';
+  static const String packageExternalRefComment = 'ExternalRefComment';
+  static const String packageAttributionText = 'PackageAttributionText';
+
 }
