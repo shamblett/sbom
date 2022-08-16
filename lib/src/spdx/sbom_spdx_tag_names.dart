249a3e70a8e27c19704c53677169b7ac915c6195
Steve Hamblett <shamblett@gtsdevlan.com>
2022-08-16 15:39:07 +0100

    Fix interpolation, update pubspec.yamls to add repository tag

--- lib/src/spdx/sbom_spdx_tag_names.dart
+++ lib/src/spdx/sbom_spdx_tag_names.dart
@@ -17,71 +17,71 @@ class SbomSpdxTagNames {
   static const String relationshipId = 'R-';
 
   /// Document creation
-  static const String version = documentCreationSectionId + 'SPDXVersion';
-  static const String dataLicense = documentCreationSectionId + 'DataLicense';
-  static const String identifier = documentCreationSectionId + 'SPDXID';
-  static const String documentName = documentCreationSectionId + 'DocumentName';
+  static const String version = '${documentCreationSectionId}SPDXVersion';
+  static const String dataLicense = '${documentCreationSectionId}DataLicense';
+  static const String identifier = '${documentCreationSectionId}SPDXID';
+  static const String documentName = '${documentCreationSectionId}DocumentName';
   static const String documentNamespace =
-      documentCreationSectionId + 'DocumentNamespace';
+      '${documentCreationSectionId}DocumentNamespace';
   static const String externalDocumentRefs =
-      documentCreationSectionId + 'ExternalDocumentRef';
+      '${documentCreationSectionId}ExternalDocumentRef';
   static const String licenseListVersion =
-      documentCreationSectionId + 'LicenseListVersion';
-  static const String creator = documentCreationSectionId + 'Creator';
-  static const String created = documentCreationSectionId + 'Created';
+      '${documentCreationSectionId}LicenseListVersion';
+  static const String creator = '${documentCreationSectionId}Creator';
+  static const String created = '${documentCreationSectionId}Created';
   static const String creatorComment =
-      documentCreationSectionId + 'CreatorComment';
+      '${documentCreationSectionId}CreatorComment';
   static const String documentComment =
-      documentCreationSectionId + 'DocumentComment';
+      '${documentCreationSectionId}DocumentComment';
 
   /// Package
-  static const String packageName = packageSectionId + 'PackageName';
-  static const String packageIdentifier = packageSectionId + 'SPDXID';
-  static const String packageVersion = packageSectionId + 'PackageVersion';
-  static const String packageFileName = packageSectionId + 'PackageFileName';
-  static const String packageSupplier = packageSectionId + 'PackageSupplier';
+  static const String packageName = '${packageSectionId}PackageName';
+  static const String packageIdentifier = '${packageSectionId}SPDXID';
+  static const String packageVersion = '${packageSectionId}PackageVersion';
+  static const String packageFileName = '${packageSectionId}PackageFileName';
+  static const String packageSupplier = '${packageSectionId}PackageSupplier';
   static const String packageOriginator =
-      packageSectionId + 'PackageOriginator';
+      '${packageSectionId}PackageOriginator';
   static const String packageDownloadLocation =
-      packageSectionId + 'PackageDownloadLocation';
-  static const String packageFilesAnalyzed = packageSectionId + 'FilesAnalyzed';
+      '${packageSectionId}PackageDownloadLocation';
+  static const String packageFilesAnalyzed = '${packageSectionId}FilesAnalyzed';
   static const String packageVerificationCode =
-      packageSectionId + 'PackageVerificationCode';
-  static const String packageChecksum = packageSectionId + 'PackageChecksum';
-  static const String packageHomePage = packageSectionId + 'PackageHomePage';
+      '${packageSectionId}PackageVerificationCode';
+  static const String packageChecksum = '${packageSectionId}PackageChecksum';
+  static const String packageHomePage = '${packageSectionId}PackageHomePage';
   static const String packageSourceInfo =
-      packageSectionId + 'PackageSourceInfo';
+      '${packageSectionId}PackageSourceInfo';
   static const String packageLicenseConcluded =
-      packageSectionId + 'PackageLicenseConcluded';
+      '${packageSectionId}PackageLicenseConcluded';
   static const String packageLicenseInfoFromFiles =
-      packageSectionId + 'PackageLicenseInfoFromFiles';
+      '${packageSectionId}PackageLicenseInfoFromFiles';
   static const String packageLicenseDeclared =
-      packageSectionId + 'PackageLicenseDeclared';
+      '${packageSectionId}PackageLicenseDeclared';
   static const String packageLicenseComments =
-      packageSectionId + 'PackageLicenseComments';
+      '${packageSectionId}PackageLicenseComments';
   static const String packageCopyrightText =
-      packageSectionId + 'PackageCopyrightText';
-  static const String packageSummary = packageSectionId + 'PackageSummary';
+      '${packageSectionId}PackageCopyrightText';
+  static const String packageSummary = '${packageSectionId}PackageSummary';
   static const String packageDescription =
-      packageSectionId + 'PackageDescription';
-  static const String packageComment = packageSectionId + 'PackageComment';
-  static const String packageExternalRef = packageSectionId + 'ExternalRef';
+      '${packageSectionId}PackageDescription';
+  static const String packageComment = '${packageSectionId}PackageComment';
+  static const String packageExternalRef = '${packageSectionId}ExternalRef';
   static const String packageExternalRefComment =
-      packageSectionId + 'ExternalRefComment';
+      '${packageSectionId}ExternalRefComment';
   static const String packageAttributionText =
-      packageSectionId + 'PackageAttributionText';
+      '${packageSectionId}PackageAttributionText';
 
   /// File
-  static const String fileFileName = fileSectionId + 'FileName';
-  static const String filePackageIdentifier = fileSectionId + 'SPDXID';
-  static const String fileFileType = fileSectionId + 'FileType';
-  static const String fileFileChecksum = fileSectionId + 'FileChecksum';
-  static const String fileLicenceConcluded = fileSectionId + 'LicenseConcluded';
+  static const String fileFileName = '${fileSectionId}FileName';
+  static const String filePackageIdentifier = '${fileSectionId}SPDXID';
+  static const String fileFileType = '${fileSectionId}FileType';
+  static const String fileFileChecksum = '${fileSectionId}FileChecksum';
+  static const String fileLicenceConcluded = '${fileSectionId}LicenseConcluded';
   static const String fileLicenseInfoInFile =
-      fileSectionId + 'LicenseInfoInFile';
-  static const String fileLicenseComments = fileSectionId + 'LicenseComments';
-  static const String fileCopyrightText = fileSectionId + 'FileCopyrightText';
+      '${fileSectionId}LicenseInfoInFile';
+  static const String fileLicenseComments = '${fileSectionId}LicenseComments';
+  static const String fileCopyrightText = '${fileSectionId}FileCopyrightText';
 
   /// Relationship
-  static const String relationship = relationshipId + 'Relationship';
+  static const String relationship = '${relationshipId}Relationship';
 }
