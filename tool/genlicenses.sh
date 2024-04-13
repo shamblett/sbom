06e4889e54493d548d2fb7d3f7f8a20b54b67d04
Steve Hamblett <steve.hamblett@linux.com>
2024-04-13 10:03:27 +0100

    Issue 14 - all tests running

--- tool/genlicenses.sh
+++ tool/genlicenses.sh
@@ -2,4 +2,9 @@
 #
 # Simple linux script to generate the all.json license file
 #
-jq -s '.' ../lib/src/spdx/licences/*.json > ../lib/src/spdx/licences/all.json
+# Assumes the SPDX license files are in json format and in the same directory
+# as this script.
+#
+# The all.json should be copied into the sbom_licenses.dart file
+#
+jq -s '.' *.json > all.json
