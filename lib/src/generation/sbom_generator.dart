29874696909b9e106ddc7c91738cfe332838adba
Steve Hamblett <steve.hamblett@linux.com>
2025-05-12 09:18:27 +0100

    Issue 21 - DCM fixes

--- lib/src/generation/sbom_generator.dart
+++ lib/src/generation/sbom_generator.dart
@@ -9,8 +9,6 @@ part of '../../sbom.dart';
 
 /// The main SBOM output generator class.
 class SbomGenerator {
-  /// Construction.
-  SbomGenerator(this.configuration);
 
   /// The valid SBOM configuration.
   final SbomConfiguration configuration;
@@ -21,12 +19,16 @@ class SbomGenerator {
   /// Output generator
   late SbomIOutputGenerator output;
 
-  /// The generated tags.
-  SbomTags get tags => output.tags;
-
   /// Path(absolute) to the generated SBOM file.
   /// Set on successful generation.
   String sbomFilePath = '';
+
+
+  /// The generated tags.
+  SbomTags get tags => output.tags;
+
+  /// Construction.
+  SbomGenerator(this.configuration);
 
   /// Generate.
   void generate() {
